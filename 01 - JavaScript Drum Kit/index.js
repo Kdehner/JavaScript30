ce8aec149695270efecae2f2fb6033455edbbaff
Kdehner <Kdehner@gmail.com>
2019-12-12 12:12:21 -0700

    Added Keypress functionality

---
+++ 01 - JavaScript Drum Kit/index.js
@@ -0,0 +1,25 @@
+window.addEventListener('load', () => {
+
+  const keys = Array.from(document.getElementsByClassName('key'));
+  const audio = Array.from(document.getElementsByTagName('audio'));
+  
+  document.addEventListener('keydown', e => {
+
+    const keyCode = String(e.keyCode);
+    const key = keys.find(key => key.dataset.key === keyCode);
+    const sound = audio.find(sound => sound.dataset.key === keyCode);
+
+    if (key) {
+      key.classList.add('playing');
+      sound.play();
+    }
+  });
+  document.addEventListener('keyup', e => {
+    const keyCode = String(e.keyCode);
+    const key = keys.find(key => key.dataset.key === keyCode);
+
+    if (key) {
+      key.classList.remove('playing');
+    }
+  });
+});
